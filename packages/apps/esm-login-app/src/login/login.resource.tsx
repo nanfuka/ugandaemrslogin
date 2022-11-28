8e6b757c31d995c8bf503966787f30f3f4d84143
nanfuka <kalungi2k6@gmail.com>
2022-11-29 01:28:16 +0300

    Fix error

--- packages/apps/esm-login-app/src/login/login.resource.tsx
+++ packages/apps/esm-login-app/src/login/login.resource.tsx
@@ -1,7 +1,6 @@
 import { openmrsFetch, refetchCurrentUser } from "@openmrs/esm-framework";
 
 export function performLogin(username, password) {
-  console.log("do you get here");
   const token = window.btoa(`${username}:${password}`);
   return openmrsFetch(`/ws/rest/v1/session`, {
     headers: {
